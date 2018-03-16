9802de40cab58580b83a2d2e110a9a732c4852bf
George <georgelroberts@hotmail.com>
2018-03-16 07:16:21 -0200

    Included absolute wins and losses.
    This shows that since 2015, the likelihood of the same outcome is double that of a different outcome.

--- WalesArsenalCorr.py
+++ WalesArsenalCorr.py
@@ -13,11 +13,13 @@ Saturday, Sunday, Monday).
 import pandas as pd
 import os
 import scipy.stats as st
+import numpy as np
 
 os.chdir('C:\\Users\\George\\OneDrive - University Of Cambridge\\Others\\Machine learning\\Wales Arsenal Correlation')
 walesData = pd.read_csv('WalesData.csv')
 walesData['Date'] = pd.to_datetime(walesData['Date'])
 walesData = walesData.drop('Match report', axis=1)
+dateFrom = 2015
 
 arsenalData = pd.read_csv('ArsenalData.csv')
 arsenalData['date'] = pd.to_datetime(arsenalData['date'], format='%d/%m/%Y')
@@ -25,7 +27,7 @@ arsenalData['date'] = pd.to_datetime(arsenalData['date'], format='%d/%m/%Y')
 
 walesData.fillna(method='ffill', inplace=True) # Only the first value of competition name is present
 walesData = walesData[walesData['Score'] != '\xa0? –\xa0?'] # Remove matches that haven't happened yet
-walesData = walesData[walesData['Date'].dt.year >= 2016]
+walesData = walesData[walesData['Date'].dt.year >= dateFrom]
 walesData = walesData[walesData['Date'].dt.dayofweek>=4] # Keep only weekends
 
 
@@ -66,7 +68,7 @@ def dateTimetoWeekend(dateList, startYear):
     weekHash = dateList.dt.week + 52 * (dateList.dt.year - startYear)
     return weekHash
 
-walesData['weekHash'] = dateTimetoWeekend(walesData['Date'], 2016)
+walesData['weekHash'] = dateTimetoWeekend(walesData['Date'], dateFrom)
 
 walesResults = walesData[['Date', 'weekHash', 'walesWin']]
 
@@ -76,7 +78,7 @@ walesResults = walesData[['Date', 'weekHash', 'walesWin']]
 
 arsenalData.dropna(axis=1, inplace=True, how='all')
 arsenalData.dropna(axis=0, inplace=True)
-arsenalData = arsenalData[arsenalData['date'].dt.year >= 2016]
+arsenalData = arsenalData[arsenalData['date'].dt.year >= dateFrom]
 arsenalData = arsenalData[arsenalData['date'].dt.dayofweek>=4] # Keep only weekends
 
 arsenalData = arsenalData[arsenalData.Results.str[0].str.isnumeric()]
@@ -97,7 +99,7 @@ def arsenalResults(dat):
 arsenalData['Winner'] = arsenalData['Results'].apply(arsenalResults)
 
 arsenalData.columns = ['Round', 'Date', 'KO', 'Place', 'Opponent','Score', 'Competition', 'arseWin']
-arsenalData['weekHash'] = dateTimetoWeekend(arsenalData['Date'], 2016)
+arsenalData['weekHash'] = dateTimetoWeekend(arsenalData['Date'], dateFrom)
 
 arseResults = arsenalData[['Date', 'weekHash', 'arseWin']]
 
@@ -106,6 +108,25 @@ mergedResults = mergedResults[mergedResults.arseWin != 2]
 mergedResults = mergedResults[mergedResults.walesWin != 2]
 
 corrs = st.pearsonr(mergedResults['walesWin'], mergedResults['arseWin'])
+
+
+def absoluteCounts(merged, dateFrom):
+    """ Prints the amount of times arsenal and wales have won together, lost
+    together, and got different results """
+    same = merged[merged['walesWin'] == merged['arseWin']]
+    different = merged[merged['walesWin'] != merged['arseWin']]
+    totalGames = merged.shape[0]
+    together = same.shape[0]
+    togetherW = same.walesWin.value_counts().loc[1]
+    togetherL = together - togetherW
+
+    print('Arsenal and Wales have played {}'.format(totalGames) +
+          ' games on the same weekend since {}.'.format(dateFrom))
+    print('Of these games, they have had the same result {}'.format(together) +
+          ' times, with {} wins and {} losses'.format(togetherW, togetherL))
+
+
+absoluteCounts(mergedResults, dateFrom)
 
 print('The Pearson R value is {:.2f} and the p-value is {:.2f}'.format(
         corrs[0], corrs[1]))
